d094cb0269829633096c7d6959e22c8ccc52e3d2
Adelchi Fratò <adelchif@gmail.com>
2015-08-14 16:47:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/adelchi/recyclerview/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.adelchi.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+    private List<Persona> persone = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+
+        for(int i=0; i<=15; i++){
+            persone.add(i, new Persona("adelchi" + i + "@gmail.com", false));
+        }
+
+        adapter = new MyAdapter(persone);
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.select_all) {
+            for (Persona persona: persone
+                 ) {
+                if(persona.getChecked()){
+                    persona.setChecked(false);
+                }else {
+                    persona.setChecked(true);
+                }
+            }
+            adapter.notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class Persona{
+
+        private String email;
+        private Boolean checked;
+
+        public Persona(String email, Boolean checked) {
+            this.email = email;
+            this.checked = checked;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Boolean getChecked() {
+            return checked;
+        }
+
+        public void setChecked(Boolean checked) {
+            this.checked = checked;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/adelchi/recyclerview/MyAdapter.java
@@ -0,0 +1,75 @@
+package com.example.adelchi.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Adelchi on 14/08/2015.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<MainActivity.Persona> persone;
+
+    private ViewHolder viewHolder;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView textView;
+        public CheckBox checkBox;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.textView = (TextView)itemView.findViewById(R.id.text);
+            this.checkBox = (CheckBox)itemView.findViewById(R.id.checkbox);
+        }
+    }
+
+    public MyAdapter(List<MainActivity.Persona> persone) {
+        this.persone = persone;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.myrow, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(v);
+
+        this.viewHolder = viewHolder;
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+        final MainActivity.Persona persona = persone.get(i);
+
+        viewHolder.textView.setText(persona.getEmail());
+
+        viewHolder.checkBox.setChecked(persona.getChecked());
+
+        viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!persona.getChecked())
+                    persona.setChecked(true);
+                else
+                    persona.setChecked(false);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return persone.size();
+    }
+
+}
